390ee6b8ec3ac17c08bba7c243afd746bb23513b
AnnaGubkina <gubkinatour@yandex>
2021-09-23 16:50:40 +0300

    changes added

--- src/WorkFile.java
+++ src/WorkFile.java
@@ -8,79 +8,49 @@ import java.util.zip.ZipOutputStream;
 
 public class WorkFile {
 
+    public static void createFolder(String obj, StringBuilder builder, String str) {
+        File newFolder = new File(obj);
+        if (newFolder.mkdir())
+            builder.append(str);
+
+    }
+
+    public static void createFile(String obj, StringBuilder builder, String str) {
+        File newFile = new File(obj);
+        try {
+            if (newFile.createNewFile())
+                builder.append(str);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
 
     public static void main(String[] args) {
 
         StringBuilder strBuilder = new StringBuilder();
 
-
         //создаем директории в папке Games
-        File src = new File("/Users/SIREN-A/Games/src");
-        boolean created = src.mkdir();
-        if (created)
-            strBuilder.append("Folder has been created \n");
+        createFolder("/Users/SIREN-A/Games/src", strBuilder, "Folder src has been created \n");
+        createFolder("/Users/SIREN-A/Games/res", strBuilder, "Folder res has been created \n");
+        createFolder("/Users/SIREN-A/Games/savegames", strBuilder, "Folder savegames has been created \n");
+        createFolder("/Users/SIREN-A/Games/temp", strBuilder, "Folder temp has been created \n");
 
-        File res = new File("/Users/SIREN-A/Games/res");
-        if (res.mkdir())
-            strBuilder.append("Folder has been created \n");
-
-        File savegames = new File("/Users/SIREN-A/Games/savegames");
-        if (savegames.mkdir())
-            strBuilder.append("Folder has been created \n");
-
-        File temp = new File("/Users/SIREN-A/Games/temp");
-        if (temp.mkdir())
-            strBuilder.append("Folder has been created \n");
-
-        //каталоге src создаем две директории: main, test
-
-        File main = new File("/Users/SIREN-A/Games/src/main");
-        if (main.mkdir())
-            strBuilder.append("Folder has been created \n");
-
-        File test = new File("/Users/SIREN-A/Games/src/test");
-        if (test.mkdir())
-            strBuilder.append("Folder has been created \n");
+        //в каталоге src создаем две директории: main, test
+        createFolder("/Users/SIREN-A/Games/src/main", strBuilder, "Folder main (in src) has been created \n");
+        createFolder("/Users/SIREN-A/Games/src/test", strBuilder, "Folder test (in src) has been created \n");
 
         //В подкаталоге main создаем два файла: Main.java, Utils.java.
-
-        File mainJava = new File("/Users/SIREN-A/Games/src/main/Main.java");
-        try {
-            if (mainJava.createNewFile())
-                strBuilder.append("Файл был создан \n");
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        File utilsJava = new File("/Users/SIREN-A/Games/src/main/Utils.java");
-        try {
-            if (utilsJava.createNewFile())
-                strBuilder.append("Файл был создан \n");
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
+        createFile("/Users/SIREN-A/Games/src/main/Main.java", strBuilder, "File Main.java has been created  \n");
+        createFile("/Users/SIREN-A/Games/src/main/Utils.java", strBuilder, "File Utils.java has been created  \n");
 
         //В каталоге res создаем три директории: drawables, vectors, icons
-        File drawables = new File("/Users/SIREN-A/Games/res/drawables");
-        if (drawables.mkdir())
-            strBuilder.append("Folder has been created \n");
-
-        File vectors = new File("/Users/SIREN-A/Games/res/vectors");
-        if (vectors.mkdir())
-            strBuilder.append("Folder has been created \n");
-
-        File icons = new File("/Users/SIREN-A/Games/res/icons");
-        if (icons.mkdir())
-            strBuilder.append("Folder has been created \n");
+        createFolder("/Users/SIREN-A/Games/res/drawables", strBuilder, "Folder drawables (in res) has been created \n");
+        createFolder("/Users/SIREN-A/Games/res/vectors", strBuilder, "Folder vectors (in res) has been created \n");
+        createFolder("/Users/SIREN-A/Games/res/icons", strBuilder, "Folder icons (in res) has been created \n");
 
         //В директории temp создаем файл temp.txt
-        File tempTXT = new File("/Users/SIREN-A/Games/temp/temp.txt");
-        try {
-            if (tempTXT.createNewFile())
-                System.out.println("Файл был создан \n");
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
+        createFile("/Users/SIREN-A/Games/temp/temp.txt", strBuilder, "File temp.txt has been created  \n");
 
         // создание FileWriter
         String text = strBuilder.toString();
@@ -115,28 +85,22 @@ public class WorkFile {
 
         // Задача 2.3 удаляем файлы, не лежащие в архиве
 
-        File fileDelete1 = new File("/Users/SIREN-A/Games/savegames/save1.dat");
-        if (fileDelete1.delete()) {
-            System.out.println("File has been deleted");
-        }
-        File fileDelete2 = new File("/Users/SIREN-A/Games/savegames/save2.dat");
-        if (fileDelete2.delete()) {
-            System.out.println("File has been deleted");
-        }
-        File fileDelete3 = new File("/Users/SIREN-A/Games/savegames/save3.dat");
-        if (fileDelete3.delete()) {
-            System.out.println("File has been deleted");
+        for (int i = 0; i < listZipFiles.size(); i++) {
+            File fileDelete = new File(listZipFiles.get(i));
+            if (fileDelete.delete()) {
+                System.out.println("File has been deleted from folder");
+            }
         }
         System.out.println();
 
-        // задача 3.1 распаковка из zip
+        //задача 3.1 распаковка из zip
 
-        openZip("/Users/SIREN-A/Games/savegames/zip.zip", "/Users/SIREN-A/Games/savegames");
+        openZip("/Users/SIREN-A/Games/savegames/zip.zip", "/Users/SIREN-A/Games/savegames/new");
 
-        // задача 3.2  считывание и десериализация одного из разархивированных файлов и вывод в консоль
+        //задача 3.2  считывание и десериализация одного из разархивированных файлов и вывод в консоль
 
-        openProgress("/Users/SIREN-A/Games/savegames/save1.dat");
-        System.out.println(openProgress("/Users/SIREN-A/Games/savegames/save1.dat"));
+        openProgress("/Users/SIREN-A/Games/savegames/newsave1.dat");
+        System.out.println(openProgress("/Users/SIREN-A/Games/savegames/newsave1.dat"));
 
 
     }
@@ -155,9 +119,9 @@ public class WorkFile {
 
         try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(wayToArchive))) {
             for (int i = 0; i < obj.size(); i++) {
-                String zipFile = obj.get(i);
+                File zipFile = new File(obj.get(i));
                 try (FileInputStream fis = new FileInputStream(zipFile)) {
-                    ZipEntry entry = new ZipEntry(zipFile);
+                    ZipEntry entry = new ZipEntry(zipFile.getName());
                     zout.putNextEntry(entry);
                     // считываем содержимое файла в массив byte
                     byte[] buffer = new byte[fis.available()];
@@ -185,7 +149,7 @@ public class WorkFile {
 
                 name = entry.getName(); // получим название файла
                 // распаковка
-                FileOutputStream fout = new FileOutputStream(name);
+                FileOutputStream fout = new FileOutputStream(unpackFolder + name);
                 for (int c = zin.read(); c != -1; c = zin.read()) {
                     fout.write(c);
                 }
